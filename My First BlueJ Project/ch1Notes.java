b160d5b0de2edc145ae68ed5c786110724192a24
Eric Gu <tianchenggu@outlook.com>
2022-08-23 12:38:57 -0500

    Started BlueJ and ch1Notes

---
+++ My First BlueJ Project/ch1Notes.java
@@ -0,0 +1,36 @@
+
+/**
+ * Eric G.
+ * 8/23/22
+ * Notes for Ch 1
+ * Where we are going to take some code notes
+ * Who helped me: Mr. Hayes, cat
+ */
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class ch1Notes
+{
+    /**
+     * main method
+     * where we are going to code
+     */
+    public static void main(String[] args)
+    {
+       Scanner keyboard = new Scanner(System.in);
+       
+       // These are the basic data types
+       int x = 9,y,z;
+       double num = 5, num1 = 23.4567, num2;
+       char letter = 'd';
+       boolean choice = false;
+       float anum = 7;
+       long a = 9;
+       short b = 5;
+       
+       String word = new String("gaming");
+       Integer number = new Integer(47);
+       Double number2 = new Double(34.5);
+    }
+}
